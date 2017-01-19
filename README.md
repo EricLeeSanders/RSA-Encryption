7555e37247f320d3234fee86d41efd5b56f6ac46
Eric Sanders <EricLeeSanders@gmail.com>
2017-01-18 19:28:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RSA-Encryption
+#### Java RSA Encryption
+
+This is an RSA Encryption that I implemented in Java using BigIntegers for my Crypto class at Butler. I had less than 24 hours to write this program. It isn't a testament to my programming ability, but shows that I have some knowledge about cryptology.
+
+##### A few notes regarding the program:
+1. The program does not handle many error exceptions and does not fail gracefully.
+2. I let the variable names be the same as those used by Thomas Barr in his book. Unfortunately, they aren't too descriptive.
+
---
+++ src/rsa/PrivateKey.java
@@ -0,0 +1,40 @@
+package rsa;
+import java.math.BigInteger;
+
+public class PrivateKey implements java.io.Serializable{
+	/**
+	 * Represents a Private Key
+	 */
+	private static final long serialVersionUID = 1L;
+	private BigInteger n, d, p, q;
+	public PrivateKey(BigInteger n, BigInteger d, BigInteger p, BigInteger q){
+		this.setN(n);
+		this.setD(d);
+		this.setP(p);
+		this.setQ(q);
+	}
+	public BigInteger getN() {
+		return n;
+	}
+	public void setN(BigInteger n) {
+		this.n = n;
+	}
+	public BigInteger getD() {
+		return d;
+	}
+	public void setD(BigInteger d) {
+		this.d = d;
+	}
+	public BigInteger getP() {
+		return p;
+	}
+	public void setP(BigInteger p) {
+		this.p = p;
+	}
+	public BigInteger getQ() {
+		return q;
+	}
+	public void setQ(BigInteger q) {
+		this.q = q;
+	}
+}
