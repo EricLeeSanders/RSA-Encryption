d5e009aa06ce4112a1c33f8589b9057d5ec731d9
Eric Sanders <EricLeeSanders@gmail.com>
2017-01-18 19:39:45 -0500

    fixed formatting issue
    fixed formatting issue

--- src/rsa/PublicKey.java
+++ src/rsa/PublicKey.java
@@ -1,28 +1,34 @@
 package rsa;
+
 import java.math.BigInteger;
 
-public class PublicKey implements java.io.Serializable{
+public class PublicKey implements java.io.Serializable {
 
-	/**
-	 * Represents a Public Key
-	 */
-	private static final long serialVersionUID = 1L;
-	private BigInteger n, e;
-	public PublicKey(BigInteger n, BigInteger e){
-		this.setN(n);
-		this.setE(e);
-	}
-	public BigInteger getN() {
-		return n;
-	}
-	public void setN(BigInteger n) {
-		this.n = n;
-	}
-	public BigInteger getE() {
-		return e;
-	}
-	public void setE(BigInteger e) {
-		this.e = e;
-	}
+    /**
+     * Represents a Public Key
+     */
+    private static final long serialVersionUID = 1L;
+    private BigInteger n, e;
+
+    public PublicKey(BigInteger n, BigInteger e) {
+        this.setN(n);
+        this.setE(e);
+    }
+
+    public BigInteger getN() {
+        return n;
+    }
+
+    public void setN(BigInteger n) {
+        this.n = n;
+    }
+
+    public BigInteger getE() {
+        return e;
+    }
+
+    public void setE(BigInteger e) {
+        this.e = e;
+    }
 
 }
